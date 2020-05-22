771632210715107cae2310e98d49effaea585319
Tonypina <65735766+Tonypina@users.noreply.github.com>
2020-05-22 11:11:52 -0500

    Update INV.h

--- INV.h
+++ INV.h
@@ -9,13 +9,12 @@
 
 #define TAM_CODE 7
 #define TAM_NAME 31
-#define TAM_QUANTITY 5
 
 typedef struct Product{
 
 	char code[TAM_CODE];
 	char name[TAM_NAME];
-	char quantity[TAM_QUANTITY];
+	int quantity;
 } Product;
 
 typedef struct Node
@@ -34,31 +33,14 @@ typedef struct INV
 } INV;
 
 INV* INV_New();
-void INV_Delete( 		INV** this 					  );
-bool INV_InsertBack( 		INV* this, char* code, char* name, char* quantity );
-bool INV_InsertFront( 		INV* this, char* code, char* name, char* quantity );
-/*
-bool INV_InsertAfter( 	INV* this, int data 			);
-bool INV_InsertBefore(	INV* this, int data 			);
-bool INV_Remove( 		INV* this, int* data_back 		);
-bool INV_RemoveFront( 	INV* this, int* data_back 		);
-bool INV_RemoveBack( 	INV* this, int* data_back 		);
-bool INV_RemoveAfter( 	INV* this, int* data_back 		);
-bool INV_RemoveBefore(	INV* this, int* data_back 		);
-*/
+void INV_Delete( 		INV** this 						);
+bool INV_InsertBack( 	INV* this, char* code, char* name, int quantity );
+bool INV_InsertFront( 	INV* this, char* code, char* name, int quantity );
+bool INV_Add(INV* this, char* code, char* name, int quantity );
+bool INV_Out(INV* this, char* code, int quantity );
 size_t INV_Len( 		INV* this 						);
 bool INV_IsEmpty( 		INV* this 						);
-void INV_MakeEmpty( 		INV* this 						);
-/*
-bool INV_Peek( 			INV* this, int* data_back 		);
-void INV_CursorFirst( 	INV* this 						);
-void INV_CursorLast( 	INV* this 						);
-void INV_CursorNext( 	INV* this 						);
-void INV_CursorPrev( 	INV* this 						);
-bool INV_FindIf( 		INV* this, int key 				);
-*/
+void INV_MakeEmpty( 	INV* this 						);
 bool INV_Search( 		INV* this, char* code 				);
-/*
-void INV_Traverse( 		INV* this, void (*pfun)(int) 	);
-*/
+
 #endif   /* ----- #ifndef INV_INC  ----- */
