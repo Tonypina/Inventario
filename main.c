fe6459f84ce0fdfe4a6d4fa9130a71a8b78862d4
rodri9 <54248087+rodri9@users.noreply.github.com>
2020-05-29 19:28:12 -0500

    Update main.c
    Hice unos cambios en el main porque no compilaba y le moví muy poco al menú, solo eso xD
    Todavía estoy viendo lo demás. Ah, y creo que haré además una prueba haciendo un módulo con funciones específicas para modificar el archivo de texto. De todos modos te mando mensaje a ver qué onda.

--- main.c
+++ main.c
@@ -13,22 +13,23 @@ void Archivo(INV* this){
 }
 
 void Menu(INV* this){
-	printf("Sistema de Inventario.\n\n");
-	printf("Opciones:\n\n");
-	printf("1. Agregar al inventario.\n");
-	printf("2. Eliminar del inventario.\n");
-	printf("3. Reportes.\n");
-	printf("4. Salir.\n");
-
+	
 	int answer = 0;
 	char code[TAM_CODE];
-	char nombre[TAM_NOMBRE];
+	char nombre[TAM_NAME];
 	int cantidad;
 	do {
-		printf("Seleccione una opción:\n");
-		scanf("%d", answer);
+		printf("Seleccione una opcion:\n");
+		printf("Sistema de Inventario.\n\n");
+		printf("Opciones:\n\n");
+		printf("1. Agregar al inventario.\n");
+		printf("2. Eliminar del inventario.\n");
+		printf("3. Reportes.\n");
+		printf("4. Salir.\n");
+
+		scanf("%d", &answer);
 		switch(answer){
-			case '1':
+			case 1:
 				printf("Inserte el codigo del producto: ");
 				gets(code);
 				printf("\nInserte el nombre del producto: ");
@@ -37,28 +38,36 @@ void Menu(INV* this){
 				scanf("%d", &cantidad);
 				INV_Add(this, code, nombre, cantidad);
 				break;
-			case '2':
+			case 2:
 				printf("Inserte el codigo del producto:");
 				gets(code);
 				printf("\nInserte la cantidad que sale:");
 				scanf("%d", &cantidad);
 				INV_Out(this, code, cantidad);
 				break;
-			case '3':
+			case 3:
+				
+				system("cls");
+				break;
+			case 4:
+				printf("---SALISTE---\n");
 
-			case '4':
-
+				system("PAUSE");
 				break;
 			default:
+				
+				printf("Ingresa una opcion valida.\n");
+				system("PAUSE");
+				system("cls");
 			
 		}
 	} while (answer != 4);
 }
 
 
-int main(){
-
-	INV inv = INV_New();
+int main()
+{	
+	INV* inv = INV_New();
 	Menu(inv);
 	INV_Delete(&inv);
 }
