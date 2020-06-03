1c93d47e8a29bd448aa12768ab3f745ae91896ab
Tonypina <65735766+Tonypina@users.noreply.github.com>
2020-06-03 17:39:30 -0500

    Update Stock.h

--- Stock.h
+++ Stock.h
@@ -4,10 +4,11 @@
 #include "INV.h"
 
 void Stock_clear_screen(                                              );
-void Stock_PrintNode( INV* this, char* name                           );
+void Stock_PrintNode( INV* this, char* code, char* name, int* quantity);
 bool Stock_Out(       INV* this, char* code, int quantity             );
 bool Stock_Add(       INV* this, char* code, char* name, int quantity );
 void Stock_Archive(   INV* this                                       );
+void Stock_ReadArchive(                                               );
 void Stock_Menu(      INV* this                                       );
 
 #endif    /* ----- #ifndef Stock_INC -----*/
